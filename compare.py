d5ca2e6dbc1f3d2cc8680a990c00808915ac5602
fanbeatsman <fan_ma8@hotmail.com>
2014-11-16 13:31:22 -0500

    first prototype

--- compare.py
+++ compare.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageChops
 from scipy.misc import imread
+import sys
+import os
 #import pprint
 
 def chunks(l, n):
@@ -12,42 +14,72 @@ point_table = ([0] + ([255] * 255))
 
 def black_or_b(a, b):
     diff = ImageChops.difference(a, b)
-    print diff
+    #print diff
     return diff
 
-a = Image.open('subject01.normal.png')
-b = Image.open('subject01.happy.png')
-c = black_or_b(a, b)
-c.save('c.png')
 
-likeness=0
+currentdir = os.getcwd()
+current_winner=["none",0]
+target = Image.open(sys.argv[2])
+
+print (os.walk(sys.argv[1]))
+for (dirpath,dirnames,filenames) in os.walk(sys.argv[1]):
+	for filename in filenames:
+		match=0
+		print (os.sep.join([dirpath, filename]))
+		#try:
+		if True:
+			candidate = Image.open(os.sep.join([dirpath, filename]))
+			difference = black_or_b(target,candidate)
+			difference.save('difference.png')
+               		difference_pixels = difference.getdata()
+               		height = difference_pixels.size[1]
+               		width = difference_pixels.size[0]
+               		difference_pixels = list(difference_pixels)
+               		difference_pixels = list(chunks(difference_pixels,3))
+                	difference_pixels = chunks(difference_pixels,width)
+			
+
+			for row in difference_pixels:
+	                        for rgb in row:
+	                                avg = sum(rgb)/3
+	                                if avg<50:
+        	                                match=match+1
+        	                                print match
+        	        if (match > current_winner[1]):
+                	        current_winner=[filename,match]
+
+
+
+		#except:
+		#	i=0
+		#	print sys.exc_info()[0]
+
+
+#a = Image.open('1_10_.gif')
+#b = Image.open('12_7_.gif')
+#c = black_or_b(a, b)
+#c.save('c.png')
+
 
 #c_pixels=list(imread("c.png").astype(int))
 
-c_pixels = c.getdata()
-height=c_pixels.size[1]
-width=c_pixels.size[0]
+#c_pixels = c.getdata()
+#height=c_pixels.size[1]
+#width=c_pixels.size[0]
 
 
-print width
-c_pixels=list(c_pixels)
-c_pixels = list(chunks(c_pixels,3))
-c_pixels = chunks(c_pixels,width)
+#print width
+#c_pixels=list(c_pixels)
+#c_pixels = list(chunks(c_pixels,3))
+#c_pixels = chunks(c_pixels,width)
 
-print c_pixels
+#print c_pixels
 
-for row in c_pixels:
-	for rgb in row:
-		print rgb
-		avg = sum(rgb)/3
-		if avg<50:
-			likeness=likeness+1
+#for row in c_pixels:
+#	for rgb in row:
+#		avg = sum(rgb)/3
+#		if avg<50:
+#			likeness=likeness+1
 
-
-print likeness
-if (likeness > 19500):
-	print ("yes")
-else:
-	print ("no")
-print width
-print height	
+print current_winner
